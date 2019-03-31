e0db7be245be95cd5ebb25c51cdbae0d6ada181c
Rebecca Pae <r.pae92@gmail.com>
2019-03-30 23:24:29 -0700

    finished adding all jquery and styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     var totalScoreCounter = 0;
     var userWinsCounter = 0;
+    var userLoseCounter = 0;
     var targetRandomNumber = 0;
     var buttonRandomNumber = [];
     var randomNumber = 0;
@@ -9,7 +10,7 @@ $(document).ready(function() {
     // To generate the random number
     function generateTargetNumber(){
         targetRandomNumber = Math.floor((Math.random() * 102) + 19);
-        var targetScore = $("<p>");
+        var targetScore = $("<div>");
         targetScore.addClass("scoreStyle");
         targetScore.text(targetRandomNumber);
         $("#random-number").append(targetScore);
@@ -20,7 +21,7 @@ $(document).ready(function() {
  
     
     //To create an array of 4 random numbers to assign to the crysal buttons
-    function generateRandomlNumbers() { 
+    function generateRandomNumbers() { 
         for (var i = 0; i < 4; i++) {
             randomNumber = Math.floor((Math.random() * 12) + 1);
             buttonRandomNumber.push(randomNumber);
@@ -29,14 +30,13 @@ $(document).ready(function() {
         return buttonRandomNumber;
     };
   
-    generateRandomlNumbers();
+    generateRandomNumbers();
     
     // To assign a random number from the buttonRandomNumber array to each button
     function assignRandomNumbers(){
         for (var i = 0; i < 4; i++) {
-            var crystalButtons = $("<button>");
+            var crystalButtons = $("<img src=../assets/images/blue-crystal-buttons.png>");
             crystalButtons.addClass("crystal-image");
-            // crystalButtons.attr("src", "../../assets/images/blue-crystal-buttons.png"); !! NEED IMAGE TAG
             crystalButtons.attr("data-crystalvalue", buttonRandomNumber[i]);
             crystalButtons.text(buttonRandomNumber[i]);
             $("#crystal-buttons").append(crystalButtons);
@@ -44,36 +44,49 @@ $(document).ready(function() {
     };
     assignRandomNumbers();
  
-    // Rest Functiom
+    // Reset Functiom
     function reset(){
         totalScoreCounter = 0;
-        userWinsCounter = 0;
+        targetRandomNumber = 0;
+        buttonRandomNumber = [];
+        randomNumber = 0;
+        
+        $("#random-number, #crystal-buttons, #user-score").empty();
         generateTargetNumber();
-        generateRandomlNumbers();
+        generateRandomNumbers();
         assignRandomNumbers();
+        console.log(buttonRandomNumber);
+
+        click();
+    };
+    
+    
+  // Calculating the user's Total Score for every click
+    function click(){
+    $(".crystal-image").on("click", function() {
+    crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    console.log(crystalValue);
+    totalScoreCounter += crystalValue;
+    console.log("total Score Counter is " + totalScoreCounter);
+    $("#user-score").text(totalScoreCounter).css({"font-size":"100px", "text-align": "center", "color": "rgb(219, 96, 157)", "font-weight": "bold"});
+
+
+
+
+    if (totalScoreCounter > targetRandomNumber) {
+        userLoseCounter += 1;
+        console.log("Lose" + userLoseCounter);
+        $("#loss-score").text(userLoseCounter).css({"font-size": "50px"});
+        reset();
     }
-    
-    // Calculating the user's Total Score for every click
-
-    $(".crystal-image").on("click", (function() {
-        var userScore = $("<p>");
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        console.log(crystalValue);
-        totalScoreCounter += crystalValue;
-        console.log("total Score Counter is " + totalScoreCounter);
-
-        if (totalScoreCounter > targetRandomNumber) {
-            alert("You Lose");
-            reset ();
-        }
-        // var userTotalScore = $("<p>");
-        // userTotalScore.addClass("scoreStyle");
-        // userTotalScore.text(totalScoreCounter);
-        // $("#user-score").append(userTotalScore);
-  
-    }));
-    
- 
-
-})
+    else if (totalScoreCounter == targetRandomNumber) {
+        userWinsCounter += 1;
+        console.log("Win" + userLoseCounter);
+        $("#wins-score").text(userWinsCounter).css({"font-size": "50px"});
+        reset();
+    }
+    });
+    }
+    click();
+});    
